585a8a34d0fdb67d64e4b486239c0cc5d73d4981
hgordon-legacy <57369652+hgordon-legacy@users.noreply.github.com>
2020-06-23 20:09:04 -0500

    updated db service

--- src/application/contracts/DatabaseServices.ts
+++ src/application/contracts/DatabaseServices.ts
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import {SnippetRepository} from '../contracts/SnippetRepository';
-export class DatabaseServices {
-  constructor() {
-  }
-
-  initDatabase() {
-    return new Promise((resolve, reject) => {
-      reject(new Error('not implemented'));
-    });
-  }
+/**
+ * Abstract database service class
+ */
+export abstract class DatabaseServices {
+/**
+ * initializes database.
+ * @returns {void} The sum of the two numbers.
+ */
+  abstract initDatabase(): void;
 }
